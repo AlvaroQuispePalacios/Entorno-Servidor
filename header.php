275fa6579b4f4839c5f055057d5fe0094c7bc5de
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2023-10-05 19:42:00 +0200

    05-10-2023

---
+++ header.php
@@ -0,0 +1,20 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="./styles.css">
+    <title>Hotel Los Perdidos</title>
+
+</head>
+<body>
+    <div class="cabecera">
+        <h1 class="titulo">Hotel Los perdidos</h1>
+        <div class="contenedor-lista">
+            <ul class="lista">
+                <li class="itemLista"><a href="./index.php">Inicio</a></li>
+                <li class="itemLista"><a href="./pages/rooms.html">Habitaciones</a></li>
+                <li class="itemLista"><a href="">Contacto</a></li>
+            </ul>
+
+        </div>
+    </div>
