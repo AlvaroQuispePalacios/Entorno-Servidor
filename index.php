e743e0f9b2e72643b68476ab2b406571f6846793
AlvaroQP <alonsol1569@gmail.com>
2023-11-14 14:02:48 +0100

    14-11-2023

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/student044/dwes/proyectoHotel/header.php') ?>
 
 <div class="container">
-    <div class="row">
+    <div class="row mt-2">
         <div class="col-12 col-sm-12 col-md-6 index-hotel__img "></div>
         <div class="col-12 col-sm-12 col-md-6 order-0 d-flex flex-column justify-content-center align-items-center p-5">
-            <h2>Hotel</h2>
+            <h2>Habitaciones</h2>
             <p class="text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem architecto cum inventore itaque in cupiditate enim. Impedit, quas! Voluptatibus dicta nostrum nobis deserunt cupiditate nulla et corrupti, quidem beatae necessitatibus?</p>
         </div>
     </div>
