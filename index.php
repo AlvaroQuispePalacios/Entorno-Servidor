d91412fe17b26d2dd4383fd3d9a7f7bb4d11a823
AlvaroQP <alonsol1569@gmail.com>
2023-11-15 00:18:57 +0100

    15-11-2023

--- index.php
+++ index.php
@@ -3,50 +3,50 @@
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/student044/dwes/proyectoHotel/header.php') ?>
 
 <!-- SLIDER -->
-<div id="carouselExampleCaptions" class="carousel slide ">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="./img/slider1.jpg" class="d-block w-100" style="height: 89vh;">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
+<div id="carouselExampleCaptions" class="carousel slide">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active" ">
+            <img src="./img/slider1.jpg" class="d-block w-100  ">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>First slide label</h5>
+                <p>Some representative placeholder content for the first slide.</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="./img/slider2.jpg" class="d-block w-100"">
+      <div class=" carousel-caption d-none d-md-block">
+            <h5>Second slide label</h5>
+            <p>Some representative placeholder content for the second slide.</p>
+        </div>
     </div>
     <div class="carousel-item">
-      <img src="./img/slider2.jpg" class="d-block w-100" style="height: 89vh;">
-      <div class="carousel-caption d-none d-md-block" >
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="./img/slider3.jpg" class="d-block w-100" style="height: 89vh;">
-      <div class="carousel-caption d-none d-md-block">
+        <img src="./img/slider3.jpg" class="d-block w-100"">
+      <div class=" carousel-caption d-none d-md-block">
         <h5>Third slide label</h5>
         <p>Some representative placeholder content for the third slide.</p>
-      </div>
     </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+</div>
+</div>
+<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+</button>
+<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
-  </button>
+</button>
 </div>
 
 <!-- CARD ROOM -->
-<div class=" p-5">
+<div class=" p-5 position-relative">
     <div class="row mt-2">
-        <div class="col-12 col-sm-12 col-md-6 index-hotel__img "></div>
-        <div class="col-12 col-sm-12 col-md-6 order-0 d-flex flex-column justify-content-center align-items-center p-5 card_room_color">
+        <div class="col-12 col-sm-12 col-md-6 card-room__img "></div>
+        <div class="col-12 col-sm-12 col-md-6 order-0 d-flex flex-column justify-content-center align-items-center p-5">
             <h2 class="fw-bold">Habitaciones</h2>
             <p class="text-center card_room_text">
                 Bienvenido a un enclave de elegancia y sofisticación, donde el lujo encuentra su máxima expresión: nuestras exclusivas habitaciones de <span class="fw-bold">Ducks Hotel</span>. Diseñadas con un toque de opulencia y atención meticulosa a cada detalle, nuestras estancias ofrecen un refugio de confort y distinción.
