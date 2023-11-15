94855fa74577269f0fd030fdc6996515d488c4cc
AlvaroQP <alonsol1569@gmail.com>
2023-11-15 11:45:06 +0100

    15-11-2023

--- index.php
+++ index.php
@@ -3,29 +3,29 @@
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/student044/dwes/proyectoHotel/header.php') ?>
 
 <!-- SLIDER -->
-<div id="carouselExampleCaptions" class="carousel slide">
+<div id="carouselExampleCaptions" class="carousel slide" >
     <div class="carousel-indicators">
         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
     <div class="carousel-inner">
-        <div class="carousel-item active" ">
-            <img src="./img/slider1.jpg" class="d-block w-100  ">
+        <div class="carousel-item active">
+            <img src="./img/slider1.jpg" class="d-block w-100 slider__img">
             <div class="carousel-caption d-none d-md-block">
                 <h5>First slide label</h5>
                 <p>Some representative placeholder content for the first slide.</p>
             </div>
         </div>
         <div class="carousel-item">
-            <img src="./img/slider2.jpg" class="d-block w-100"">
+            <img src="./img/slider2.jpg" class="d-block w-100 slider__img">
       <div class=" carousel-caption d-none d-md-block">
             <h5>Second slide label</h5>
             <p>Some representative placeholder content for the second slide.</p>
         </div>
     </div>
     <div class="carousel-item">
-        <img src="./img/slider3.jpg" class="d-block w-100"">
+        <img src="./img/slider3.jpg" class="d-block w-100 slider__img">
       <div class=" carousel-caption d-none d-md-block">
         <h5>Third slide label</h5>
         <p>Some representative placeholder content for the third slide.</p>
@@ -43,7 +43,7 @@
 </div>
 
 <!-- CARD ROOM -->
-<div class=" p-5 position-relative">
+<div class="p-4 p-md-5">
     <div class="row mt-2">
         <div class="col-12 col-sm-12 col-md-6 card-room__img "></div>
         <div class="col-12 col-sm-12 col-md-6 order-0 d-flex flex-column justify-content-center align-items-center p-5">
