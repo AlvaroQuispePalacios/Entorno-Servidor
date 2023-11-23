fbcf45b58ce4f2332a87349a5ee364a8ddcfce9d
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2023-11-23 19:44:50 +0100

    23-11-2023(insert img room_category)

--- db/room_category/db_room_category_insert.php
+++ db/room_category/db_room_category_insert.php
@@ -7,24 +7,40 @@
 
         // Cambialo a un fichero aparte porque vas a utilizarlo para que un cliente pueda subir una foto de perfil, para que puedas ingresar las fotos de una nueva categoria de habitacion y si se quiere foto de perfil del administrador
         if(isset($_FILES['file'])){
+
             $file = $_FILES['file'];
+
             // Nombre del archivo subido
             $file_name = $file['name'];
+
             // Con el tipo luego podemos filtrarlo para que sea solo formatos png, jpg, jpge
             $file_type = $file['type'];
-
-            print_r($file);
-            echo "<br>";
-            echo $file_name;
-            echo "<br>";
-            echo $file_type;
-            echo "<br>";
-            echo $file['tmp_name'];
             
-            move_uploaded_file($file['tmp_name'], $_SERVER['DOCUMENT_ROOT'].'/student044/dwes/img/'.$file_name);
+            $valid_types = array('image/jpg', 'image/webp', 'image/png', 'image/jpge');
 
-        }else{
-            echo 'El archivo no existe D:';
+            // Variable donde guardaremos la ruta de la imagen en la base de datos, si esta vacia se añadira una imagen por defecto 
+            $path_img = '/student044/dwes/img/default.png';
+
+            // Comprobar que el archivo sea jpg, webp, png, jpge y guardamos la ruta de la imagen para agregarlo a la base de datos
+            if(in_array($file_type, $valid_types)){
+                $path_img = '/student044/dwes/img/'.$file_name;
+                move_uploaded_file($file['tmp_name'], $_SERVER['DOCUMENT_ROOT'].'/student044/dwes/img/'.$file_name);
+            }else {
+                echo "Imagen no existe o no es valido, se utilizara una imagen por defecto, luego se podra cambiar la imagen";
+            }
         }
+
+        //Conexion a la base de datos
+        include($_SERVER['DOCUMENT_ROOT'].'/student044/dwes/db/db_connect.php');
+
+        $sql = "INSERT INTO 044_room_category
+        (room_category_id, room_category_name, room_category_price, room_category_description, room_category_img)
+        VALUES
+        (DEFAULT, '$room_category_name', '$room_category_price', '$room_category_description', )";
+
+        // $query = mysqli_query($con, $sql);
+
+        
+
     }
 ?>
