0f8ab2d8193f4e16d8d62bcb9387b0ce482cd282
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2023-11-24 18:16:39 +0100

    24-11-2023

--- db/customer/db_customer_delete.php
+++ db/customer/db_customer_delete.php
@@ -19,9 +19,12 @@
             -Añadir que se puedan subir  imagenes (Administrador podria subir imagenes de la habitacion) Para hacert esto necesito ver las diferente categorias hacer(SELECT, INSERT, UPDATE, DELETE; cuando haga insert de las habitaciones que el administrador pueda subir imagenes de la nueva categoria)
         */
         /* SECUNDARIO
+
             -RECUERDA: Vas a agregar la columna customer_status(enabled, disabled)--> Con esto el delete sera pasar el estado de enabled a disabled y no hacer un delete
             
             -RECUERDA: Que un usuario con la el estado "disabled" no puede reservar habitaciones, Si quiere activar su un administrador tiene que hacerlo manualmente(AGREGA UNA CONDICION )
+
+            -COOKEIS --> Idioma, guardar las fechas de reserva cuando no estamos logeados y le damos al boton de reservar nos mande a hacer login seria bonito que la fechas ya esten rellenadas o mejor que ya reserven con todos los datos   
         */
         if($resultado){
             echo "El cliente ha sido eliminado";
