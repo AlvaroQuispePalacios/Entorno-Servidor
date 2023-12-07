5ac4f30d42f650ec81e83abc803e3e5ef58eb9f9
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2023-12-07 19:20:08 +0100

    07-12-2023

--- db/db_log_out.php
+++ db/db_log_out.php
@@ -3,5 +3,6 @@
     unset($_SESSION['user_name']);
     unset($_SESSION['user_id']);
     unset($_SESSION['user_rol']);
+    session_destroy();
     header('Location: /student044/dwes/index.php');
 ?>
