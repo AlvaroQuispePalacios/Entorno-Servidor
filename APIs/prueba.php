04225879cf9ea094ae8482d3759ab1efe20b63a9
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2024-01-23 21:09:13 +0100

    23-01-2024

--- APIs/prueba.php
+++ APIs/prueba.php
@@ -1,21 +1,15 @@
-<?php include($_SERVER['DOCUMENT_ROOT'] . '/student044/dwes/header.php') ?>
+
 
 
 <script>
-
-    let http = new XMLHttpRequest();
-    function pruebaAPI() {
+    function showTime() {
+        let http = new XMLHttpRequest();
         http.onreadystatechange = () => {
             if (http.readyState == 4 && http.status == 200) {
-
-                document.getElementById("searchName").innerHTML = http.responseText;
                 console.log(http.responseText);
-
             }
         };
-        http.open("GET", "/student044/dwes/db/customer/ajax_db_customer_select.php?q=" + str, true);
+        http.open("GET", "http://dataservice.accuweather.com/currentconditions/v1/" + 305482, true);
         http.send();
-
     }
 </script>
-<?php include($_SERVER['DOCUMENT_ROOT'] . '/student044/dwes/header.php') ?>
--- index.php
+++ index.php
@@ -1,58 +1,76 @@
-
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/student044/dwes/header.php') ?>
 
- <!-- SLIDER -->
- <div id="carouselExampleCaptions" class="carousel slide">
-      <div class="carousel-indicators">
+<!-- SLIDER -->
+<div id="carouselExampleCaptions" class="carousel slide">
+    <div class="carousel-indicators">
         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-      </div>
-      <div class="carousel-inner">
+    </div>
+    <div class="carousel-inner">
         <div class="carousel-item active">
-          <img src="./img/slider1.jpg" class="d-block w-100 slider__img">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>First slide label</h5>
-            <p>Some representative placeholder content for the first slide.</p>
-          </div>
+            <img src="./img/slider1.jpg" class="d-block w-100 slider__img">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>First slide label</h5>
+                <p>Some representative placeholder content for the first slide.</p>
+            </div>
         </div>
         <div class="carousel-item">
-          <img src="./img/slider2.jpg" class="d-block w-100 slider__img">
-          <div class=" carousel-caption d-none d-md-block">
-            <h5>Second slide label</h5>
-            <p>Some representative placeholder content for the second slide.</p>
-          </div>
+            <img src="./img/slider2.jpg" class="d-block w-100 slider__img">
+            <div class=" carousel-caption d-none d-md-block">
+                <h5>Second slide label</h5>
+                <p>Some representative placeholder content for the second slide.</p>
+            </div>
         </div>
         <div class="carousel-item">
-          <img src="./img/slider3.jpg" class="d-block w-100 slider__img">
-          <div class=" carousel-caption d-none d-md-block">
-            <h5>Third slide label</h5>
-            <p>Some representative placeholder content for the third slide.</p>
-          </div>
+            <img src="./img/slider3.jpg" class="d-block w-100 slider__img">
+            <div class=" carousel-caption d-none d-md-block">
+                <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p>
+            </div>
         </div>
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
-      </button>
-    </div>
-    
-    <!-- CARD ROOM -->
-    <div class="p-4 p-md-5">
-      <div class="row mt-2">
+    </button>
+</div>
+
+<!-- CARD TIME -->
+<div>
+    <button onclick="showTime()">Llamar a la API</button>
+</div>
+<!-- CARD ROOM -->
+<div class="p-4 p-md-5">
+    <div class="row mt-2">
         <div class="col-12 col-sm-12 col-md-6 card-room__img "></div>
         <div class="col-12 col-sm-12 col-md-6 order-0 d-flex flex-column justify-content-center align-items-center p-5">
-          <h2 class="fw-bold">Habitaciones</h2>
-          <p class="text-start card_room_text">
-            Bienvenido a un enclave de elegancia y sofisticación, donde el lujo encuentra su máxima expresión: nuestras exclusivas habitaciones de <span class="fw-bold">Ducks Hotel</span>. Diseñadas con un toque de opulencia y atención meticulosa a cada detalle, nuestras estancias ofrecen un refugio de confort y distinción.
-          </p>
+            <h2 class="fw-bold">Habitaciones</h2>
+            <p class="text-start card_room_text">
+                Bienvenido a un enclave de elegancia y sofisticación, donde el lujo encuentra su máxima expresión: nuestras exclusivas habitaciones de <span class="fw-bold">Ducks Hotel</span>. Diseñadas con un toque de opulencia y atención meticulosa a cada detalle, nuestras estancias ofrecen un refugio de confort y distinción.
+            </p>
         </div>
-      </div>
     </div>
-    <html lang="es">
+</div>
+
+
+<script>
+    function showTime() {
+        let http = new XMLHttpRequest();
+        http.onreadystatechange = () => {
+            if (http.readyState == 4 && http.status == 200) {
+                let json = JSON.parse(http.responseText);
+                console.log(json);
+            }
+        };
+        http.open("GET", "http://dataservice.accuweather.com/currentconditions/v1/305482?apikey=tw93fBWL0LAm3HS0NbvgkHjH0rpv0Nlz", true);
+        http.send();
+    }
+
+</script>
 
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/student044/dwes/footer.php') ?>
