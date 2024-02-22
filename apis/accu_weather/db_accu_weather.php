b4b048f028e6d5acddae8c6100026c8697326e42
AlvaroQuispePalacios <aquispe19975@iesjoanramis.org>
2024-02-22 19:41:26 +0100

    22-02-2024

--- apis/accu_weather/db_accu_weather.php
+++ apis/accu_weather/db_accu_weather.php
@@ -2,45 +2,54 @@
 
 // Conexion a la base de datos
 include($_SERVER['DOCUMENT_ROOT'] . '/student044/dwes/db/db_connect.php');
+$url_icons = $_SERVER['DOCUMENT_ROOT'] . '/student044/dwes/apis/accu_weather/files/accu_weather_icons.json';
+$icons = json_decode(file_get_contents($url_icons), true);
 
-$sql = "SELECT * FROM 044_accu_weather
-        WHERE DATE(accu_weather_inserted_on) >= CURDATE() - INTERVAL 7 DAY 
-        AND accu_weather_inserted_on = (
-            SELECT MAX(accu_weather_inserted_on) 
-            FROM 044_accu_weather 
-            WHERE DATE(accu_weather_inserted_on) = DATE(accu_weather_inserted_on)
-        )
-        GROUP BY DATE(accu_weather_inserted_on)
-        ORDER BY MAX(accu_weather_inserted_on) DESC;";
+$sql = "SELECT * FROM 044_accu_weather ORDER BY `accu_weather_inserted_on` DESC LIMIT 7";
+
+// SELECT * FROM `044_accu_weather` as acu 
+// GROUP BY EXTRACT(DAY FROM `accu_weather_inserted_on`)
+// ORDER BY `accu_weather_inserted_on` DESC;
 
 $query = mysqli_query($con, $sql);
 $accu_weather = mysqli_fetch_all($query, MYSQLI_ASSOC);
 
-$accu_weather_decode = json_decode($accu_weather[0]["accu_weather_json"], true);
-print_r($accu_weather_decode);
 ?>
 
 <article class="p-4 p-md-5">
     <section class="row mt-2 d-flex justify-content-center">
         <h2 class="d-flex justify-content-center fw-bold">Tiempo</h2>
         <div class="col-12 col-sm-12 col-md-12 order-0 py-5 px-0 d-flex justify-content-center gap-5">
-            <?php foreach($accu_weather_decode as $current_conditions)?>
-            <div class="card-time">
-                <div class="card-time-content">
-                    <div class="card-time-img">
-                        <img src="/student044/dwes/img/freddy.webp" alt="Imagen del tiempo">
-                    </div>
-                    <div class="card-time-text">
-                        <span>
-                            <?php print_r(date("l", strtotime($current_conditions["LocalObservationDateTime"])));?>
-                        </span>
-                        <?php print_r($current_conditions["Temperature"]["Metric"]["Value"]) . " " . print_r($current_conditions["Temperature"]["Metric"]["Unit"]) ?>
+            <?php foreach ($accu_weather as $item) { ?>
+                <?php
+                $current_conditions = json_decode($item["accu_weather_json"], true);
+                ?>
+                <div class="card-time">
+                    <div class="card-time-content">
+                        <div class="card-time-img">
+                            <?php foreach ($icons as $icon) {
+                                if ($icon["IconNumber"] == $current_conditions[0]["WeatherIcon"]) {
+                            ?>
+                                    <img src="<?php print_r($icon["icon"]) ?>" alt="Imagen del tiempo">
+                                <?php } ?>
+                            <?php } ?>
+                        </div>
+                        <div class="card-time-text">
+                            <span>
+                                <?php print_r(date("l", strtotime($current_conditions[0]["LocalObservationDateTime"]))); ?>
+                            </span>
+                            <?php print_r($current_conditions[0]["Temperature"]["Metric"]["Value"]) . " " . print_r($current_conditions[0]["Temperature"]["Metric"]["Unit"]) ?>
 
-                        <span>
-                            <?php print_r($current_conditions["WeatherText"]) ?>
-                        </span>
+                            <span>
+                                <?php print_r($current_conditions[0]["WeatherText"]) ?>
+                            </span>
+                        </div>
                     </div>
                 </div>
-            </div>
+            <?php } ?>
+
+
+
+
     </section>
 </article>
